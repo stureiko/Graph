c24a728b7b283c283555d546444041a68b3a3b7d
Igor Stureiko <stureiko@mail.ru>
2020-08-23 16:22:27 +0300

    Убрал лишние скрипты, переименовал переменную graph -> graphNodes

--- js/lifeExpectancy.js
+++ js/lifeExpectancy.js
@@ -3,13 +3,14 @@ import {tableFactory, coloredTable} from "./tableFactory.js";
 export default async function lifeExpectancy(){
     const my_data = getJsonData();
     console.log(my_data);
-    let graph = [["Id", "Node name", "x", "y", "consumption"]];
+    let graphNodes = [["Id", "Node name", "x", "y", "consumption"]];
 
     for (let i=0; i < my_data.nodes.length; i++){
-        graph.push([my_data.nodes[i].id, my_data.nodes[i].name, my_data.nodes[i].x, my_data.nodes[i].y, my_data.nodes[i].consumption]);
+        graphNodes.push([my_data.nodes[i].id, my_data.nodes[i].name,
+                    my_data.nodes[i].x, my_data.nodes[i].y, my_data.nodes[i].consumption]);
     }
 
-    tableFactory(graph);
+    tableFactory(graphNodes);
     coloredTable();
 }
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-import testScript from "./scriptTest.js";
 import addGraph from "./graph.js";
 import lifeExpectancy from "./lifeExpectancy.js";
 
 function main(){
-    testScript();
     // addGraph();
     lifeExpectancy();
 }
