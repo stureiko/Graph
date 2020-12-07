1f1691b38ba091a9b46b404db1885ef95bf4fd3c
Igor Stureiko <stureiko@mail.ru>
2020-12-07 22:09:08 +0300

    Добавлена загрузка графа на поле отрисовки

--- js/lifeExpectancy.js
+++ js/lifeExpectancy.js
@@ -1,23 +1,9 @@
 import {tableFactory, coloredTable} from "./tableFactory.js";
 
-export default async function lifeExpectancy(){
-    const my_data = getJsonData();
-    console.log(my_data);
-    let graphNodes = [["Id", "Node name", "x", "y", "consumption"]];
 
-    for (let i=0; i < my_data.nodes.length; i++){
-        graphNodes.push([my_data.nodes[i].id, my_data.nodes[i].name,
-                    my_data.nodes[i].x, my_data.nodes[i].y, my_data.nodes[i].consumption]);
-    }
+export default async function lifeExpectancy(graphNodes){
 
     tableFactory(graphNodes);
     coloredTable();
 }
 
-function getJsonData(){
-    const jqXHR = $.ajax({
-        url: "data/data.json",
-        async: false});
-
-    return $.parseJSON(jqXHR.responseText);
-}
--- js/main.js
+++ js/main.js
@@ -2,9 +2,26 @@ import addGraph from "./graph.js";
 import lifeExpectancy from "./lifeExpectancy.js";
 
 function main(){
-    // addGraph();
-    lifeExpectancy();
+    const my_data = getJsonData();
+    console.log(my_data);
+
+    let graphNodes = [["Id", "Node name", "x", "y", "consumption"]];
+
+    for (let i=0; i < my_data.nodes.length; i++){
+        graphNodes.push([my_data.nodes[i].id, my_data.nodes[i].name,
+            my_data.nodes[i].x, my_data.nodes[i].y, my_data.nodes[i].consumption]);
+    }
+
+    addGraph(graphNodes);
+    lifeExpectancy(graphNodes);
 }
 
+function getJsonData(){
+    const jqXHR = $.ajax({
+        url: "data/data.json",
+        async: false});
+
+    return $.parseJSON(jqXHR.responseText);
+}
 
 $(document).ready(main);
